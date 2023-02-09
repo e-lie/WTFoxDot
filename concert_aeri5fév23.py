0017860ce41078fd28a3577a313a984be637e16d
Elie Gavoty <eliegavoty@free.fr>
2023-02-09 19:43:22 +0100

    pre_eart

--- concert_aeri5fév23.py
+++ concert_aeri5fév23.py
@@ -16,65 +16,136 @@ change_bpm(110, True, 0.22)
 Scale.default = Scale.minor
 
 Root.default = 0
-Root.default = var([0,2,5,4], 16)
-Root.default = var(PTri(12), 4)
-Root.default = var([0,2], 32)
 
-s1 >> space([0], dur=[.5,.25,.25], oct=5, amp=[.8,.7,.8,1.1]).mpan(0)
+a1 >> apad([0,4,-2], dur=PRand(2,8), sus=PRand(4,12), vol=1.1, attack=.4, space=0, detail=0, thick_thin=0).span(.5).fadein(32)
+
+a1.span(srot(24))
+
+a1.space=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+a1.thick_thin=linvar([0,1,.4,.8,0,.6], PRand(2,24), start=Clock.mod(4))
+a1.detail=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+
+a1 + (0,12)
+
+a4 >> gone([0], dur=4, dull=0, body=0, arp=0, pitch=0).fadein(16, i)
+
+a4.dull = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+a4.body = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+# a4.pitch = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+a4.pitch = .5#linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+a4.pitch = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+
+a4.arp = 0# linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+a4.arp = 0
+a4.gone_arp = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
+a4.oct=3
+a4.span(srot(32))
+
+a1.vol = linvar([1,0], 16, start=Clock.mod(4))
+
+a4.vol = linvar([1,0], 16, start=Clock.mod(4))
+
+a1.stop()
+
+s1 >> space([0], dur=[.5,.25,.25], oct=5, amp=[.8,.7,.8,1.1], sus=s1.dur+0.2).mpan(0)
 
 s1.mpan([0,2])
 
-s1.dur=P[P[.5, .5, .5, P*(.25,.25)],.25,.25]
-
 s1.mpan(P[0,2].stutter(3))
 
-s1.mpan(P[0,2,3,1])
-
 s1.oct=(4,5)
+
+s1.dur=P[P[.5, .5, .5, P*(.25,.25)],.25,.25]
 
 s1.mpan(mrot(6).shuffle().stutter(2))
 
 s1.oct=(4,5,3)
 
-s1.oct=P[P(4,5,3,6), (4,5), (3,6), 5, 6, 3]
-
 s2 >> blip([0], dur=[.5,.25,.25], oct=5, amp=P[.8,.7,.8,1.1]*2).mpan(0)
 
-s2.sus=1.2
+s2.mpan(3)
+
+s2.sus=linvar([.2,1.5], PRand(4,16))
+
+s1.oct=P[P(4,5,3,6), (4,5), (3,6), 5, 6, 3]
+
+s1.oct=Pvar([P[P(4,5,3,6), (4,5), (3,6), 5, 6, 3], [3,4,5,6], P(3,4,5,6)], 8)
+
+Root.default = var(PTri(12), 4, start=Clock.mod(4))
 
 s2.mpan(P[0,2,3,1].stutter(9))
+
+Root.default = var(PTri(12), .25)
+
+Root.default = var(PTri(12), 4)
 
 s2.dur=PDur(3,7)
 
 s2.oct = P[5,5,5,6,4]
 
+s1.fadeout(16)
+
+s2.only()
+
+bpm_to(110)
+change_bpm(115, True, 0.22)
+
+s2.oct=(3,4)
+s2.sus=2
 
 # TODO make all the multiparams progressive andd musical
 a1 >> padarp(
     [0, 2],
-    # dur=[2,1.2,1.8, 1],
     dur=[1,.5, 2],
     oct=3,
     amp=.8,
-    # verb=0,
-    verb=linvar([0,1], [32,inf], start=Clock.mod(4)),
-    # delay=0,
-    delay=linvar([0,1], [64,inf], start=Clock.mod(4)),
+    verb=0,
+    delay=0,
     detune=sinvar([0,1], PWhite(.2,3)[:16]),
-    # expand=linvar([0, 1], 16),
     expand=0,
     vol=.7,
-).span(expvar([0,1,-1,3,0,5], PRand(2,8)))
+).span(expvar([0,1,-1,3,0,5], PRand(8,32))).fadein(16)
+
+a1.verb=linvar([0,1], [32,inf], start=Clock.mod(4)),
+
+a1.delay=linvar([0,1], [64,inf], start=Clock.mod(4)),
+
+a1.expand=linvar([0, 1], 16),
+
+Root.default = 0
 
 d1 >> play(
-    "<x.><{~~-v}{--[--]}~(=~)>",
-    dur=var([.5, .25, .5, .165], [6, 6, 3, 1]),
-    amp=1.5,
-    rate=P[1.2, 1.5, 1]*linvar([.7,2], 32),
+    # ".~",
+    # "(.i)~",
+    "(.i)(~~[--]=)",
+    # dur=[.45,.5,.55],
+    # dur=var([.5, .25], [7,1]),
+    dur=P[.45,.5,.55]*var([1, .5], [7,1]),
+    amp=1.5*PWhite(.8,1.2),
+    # rate=PWhite(.95,1.05),
+    rate=.7,
+    # rate=P[1.2, 1.5, 1]*linvar([.5,1], 32),
+    # cut=.5,
+    cut=2,
+    room2=.3,
+    crush=1,
+    bits=5,
+    # lpf=4000,
+    # hpf=800,
+).mpan(mrot(64))
+
+d2 >> play(
+    "<([xx]xx.)(..x)>",
+    # "x.",
+    dur=.5,
+    amp=PWhite(1,1.5, seed=3)[:7]|P[0],
+    rate=P[1.2, 1.5, 1]*linvar([.5,1], 32),
+    # rate=1,
     cut=.8,
-    # cut=2,
-    room2=0,
-).mpan(mrot(32))
+    output=12,
+    sample=3,
+    pan=0,
+)
 
 a2 >> padarp(
     [0, 2,0,5],
@@ -84,9 +155,26 @@ a2 >> padarp(
     amp=.8
 )
 
-## TODO Where to put that
+s1.fadeout(32)
+
+s2.fadeout(32)
+
+b1 >> blip(
+    PWalk(start=0, seed=10)[:64] * 2,
+    dur=.25,
+    # dur=var([.25, 1/3, .5, 1/6], [6,2,6,2]),
+    # dur=Pvar([P[.45, .55, .5, .5, .48, .52]/2, P[1 / 3, 2 / 3]/2], [12, 4]),
+    sus=PWhite(.5,2),
+    amp=PWalk(start=30, seed=10)[:64] / 30 + .5,
+    oct=var([4,5,6,7,6,5],8),
+    # pan=var([-1,.5,0,1,-.5], PRand(1,4)/2)
+).mpan(PRand(0,5))
+
+b1 + P[0, 0, 0, 1, 1, 0, -2, 0, 1] 
+
+b1 + P*(0,2)
+
 Scale.default = Pvar([Scale.minor, Scale.major, Scale.minor, Scale.majorPentatonic, Scale.major], PRand(1,4)[:32]*4)
-Scale.default = Scale.majorPentatonic
 
 # k1 >> play("c.{..c+}{vccc+}(v{vv+cc})", dur=[.4,.3,.3], amp=2).pause(0,24).mpan(PRand(0,1))
 
@@ -99,6 +187,8 @@ b1 >> marimba(
     vol=.8,
 ).span(srot(16)).pause(12,16)
 
+b1.stop()
+
 b1 >> marimba([0,2,1,-2], dur=[.25,.25,.3,.4,.4], sus=1, oct=P[3,4,6].stutter(8), amp=PWalk(start=30, seed=13)[:30]/30).span(srot(16))
 b2 >> marimba([4,4,5,6], dur=[.5,.3,.4,.4], sus=1, oct=P[4,5,7].stutter(7), amp=PWalk(start=30, seed=11)[:30]/30).span(srot(16))
 
@@ -106,24 +196,8 @@ b1.stop()
 
 d2 >> play(".---", sdb=1, sample=0, hpf=2000, dur=1/4, leg=3, amp=2).pause(2,7)
 
-b1 >> blip(
-    PWalk(start=0, seed=10)[:64] * 2,
-    dur=.25,
-    # dur=Pvar([P[.45, .55, .5, .5, .48, .52]/2, P[1 / 3, 2 / 3]/2], [12, 4]),
-    sus=2,
-    amp=PWalk(start=30, seed=10)[:64] / 30 + 1,
-    oct=var([4,5,6,7,6,5],8),
-    # pan=var([-1,.5,0,1,-.5], PRand(1,4)/2)
-).mpan(PRand(0,5)) + [0, 0, 0, 1, 1, 0, -2, 0, 1]
 
 a2.fadeout(32)
-
-a1 >> apad([0,4,-2], dur=PRand(2,8), sus=PRand(4,12), vol=1.1).span(srot(12))
-
-a1.apad_attack=.4
-a1.apad_space=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a1.apad_thick_thin=linvar([0,1,.4,.8,0,.6], PRand(2,24), start=Clock.mod(4))
-a1.apad_detail=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
 
 a1.sampfadeout(32)
 
@@ -146,17 +220,11 @@ a2.fordrip_ambiance=1
 a2.fadeout(32)
 a1.fadeout(32)
 
-a4 >> gone([0], dur=4)
-a4.gone_dull = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a4.gone_body = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-# a4.gone_pitch = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a4.gone_pitch = .5#linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a4.gone_pitch = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a4.gone_arp = 0# linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a4.arp = 0
-a4.gone_arp = linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
-a4.oct=2
-a4.span(srot(32))
+
+
+a4.fadeout(16)
+
+a4.vol=.7
 
 a4.only()
 
@@ -194,7 +262,7 @@ k1 >> kicker("<v.>", dur=.5, amp=1, crush=4, bits=16, rate=1, output=12)
 
 k2 >> kicker("<(VVV(V[.V]V[VV]))(...V)>", dur=.5, amp=1, crush=8, bits=linvar([6,2],24, start=Clock.mod(4)), output=12)
 
-d7 >> bbass(chords, dur=PSum(3,2), oct=(2,3), amp=1.5, output=12) + [0,0,2] + [0,0,0,0,-1]
+d7 >> bbass(chords, dur=PSum(3,2), oct=(2,3), amp=1, output=12) + [0,0,2] + [0,0,0,0,-1]
 
 d7.pitch = PRand(1,10)
 
@@ -207,7 +275,7 @@ k2.stop()
 
 bpm_to(130)
 
-change_bpm(140)
+change_bpm(120, True, 0.22)
 
 
 e3.stop()
@@ -257,6 +325,7 @@ k1 >> kicker(Pvar(["<.X><.v>",'.','<X.><v.>','.'], [44,4]), dur=1/2, sdb=1, samp
 k1 >> play(Pvar(["<.X><.v>",'.','<X.><v.>','.'], [44,4]), dur=1/2, sample=1, amp=1.5)
 
 d3 >> play("<...c...c.><...(...*)><..(i).>", dur=1/4, rate=var([1,1.3]), sample=1, amp=3).mpan(mrot(32))
+
 b3 >> blip(pitches, dur=P[.5,.5,.25,.5,.25,.5,.25,.5,.5,.25]*1, sus=b1.dur-.04, amp=3).mpan(mrot(32))
 
 d2 >> play(".---", sdb=1, sample=0, hpf=2000, dur=1/4, leg=3, amp=2).pause(2,7).mpan(mrot(32))
@@ -285,7 +354,7 @@ b2 >> bbass(P[4,2,0].stutter(2), oct=(3,5), dur=[1,1,2], amplify=1)
 
 d8 >> play("/", dur=16, pan=[-1, 0, -1]).mpan(mrot(32))
 
-e1 >> play("*", sample=2, dur=1/4, amp=PRand(2)[:16], amplify=pa16_3).mpan(mrot(24))
+e1 >> play("*", sample=2, dur=1/4, amp=PRand(2)[:16]).mpan(mrot(24))
 
 d2 >> play("V", dur=clave23, sample=1, amp=1.5).mpan(mrot(32)).only()
 
