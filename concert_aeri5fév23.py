13e898bba811d856927edf9bebbef5e4856381a7
Elie Gavoty <eliegavoty@free.fr>
2023-02-11 14:05:21 +0100

    post_eart_concert

--- concert_aeri5fév23.py
+++ concert_aeri5fév23.py
@@ -60,6 +60,8 @@ a1.fadeout(16, ivol=.7)
 
 a1.stop()
 
+a4.stop()
+
 s1 >> space([0], dur=[.5,.25,.25], oct=5, amp=P[.8,.7,.8,1.1]*1.5, sus=s1.dur+0.2).mpan(0)
 
 s1.mpan([0,2])
@@ -79,6 +81,10 @@ s1.mpan(mrot(6).shuffle().stutter(2))
 s1.oct=(4,5,3)
 
 s2 >> blip([0], dur=[.5,.25,.25], oct=5, amp=P[.8,.7,.8,1.1]*2).mpan(0)
+
+s2.degree = [0,2]
+
+s2.degree = PWalk()
 
 s2.mpan(3)
 
@@ -180,6 +186,8 @@ k1 >> play(
 
 k1.degree = "<([xx]xx.)(..x)>",
 
+k1.degree = "x."
+
 k1.pause(8,32)
 
 k1.dur = PDur(3,8)
@@ -206,7 +214,7 @@ b1 >> blip(
     # dur=var([.25, 1/3, .5, 1/6], [6,2,6,2]),
     # dur=Pvar([P[.45, .55, .5, .5, .48, .52]/2, P[1 / 3, 2 / 3]/2], [12, 4]),
     sus=PWhite(.5,2),
-    amp=PWalk(start=30, seed=10)[:64] / 30 + .5,
+    amp=PWalk(start=30, seed=10)[:64] / 30 + .8,
     oct=var([4,5,6,7,6,5],8),
     # pan=var([-1,.5,0,1,-.5], PRand(1,4)/2)
 ).mpan(PRand(0,5))
@@ -224,7 +232,15 @@ b2 >> marimba(
     oct=P[3, 4, 6].stutter(8),
     amp=PWalk(start=30, seed=13)[:30] / 30,
     vol=.8,
+).span(srot(16)b2 >> marimba(
+    [0, 2, 1, -2],
+    dur=[.3, .4, .4],
+    sus=1,
+    oct=P[3, 4, 6].stutter(8),
+    amp=PWalk(start=30, seed=13)[:30] / 30,
+    vol=.8,
 ).span(srot(16))
+)
 
 k1.dur=.5
 
@@ -253,7 +269,7 @@ m2 >> play(
     rate=var([1, 1.5, .4], 7)
 ).pause(2, 7)
 
-m2.mpan(var([0,3,1,4,2,5]), 7)
+m2.mpan(var([0,3,1,4,2,5], 7))
 
 a2 >> fordrip(
     [0],
@@ -268,10 +284,16 @@ a2 >> fordrip(
 
 a2.oct=[2,3,5,7]
 
+k1.stop()
+
 a2.bitcrush=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
 a2.drip=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
 a2.psy=1
 a2.ambiance=1
+
+a2.sampfadeout(16)
+
+a2.only()
 
 m1 >> marimba("ab[d][cc]a.........b[aaaa]([c]a[cccc]d)[ff]", oct=3, amp=linvar([.1,.9],16, bus2=.5), dur=.125).humz().span(srot(64)).fadein()
 
@@ -304,6 +326,10 @@ m3.ampfadeout()
 
 m3.stop()
 
+a2.stop()
+
+m2.stop()
+
 b2 >> blip(chords, dur=clave23, sus=linvar([1,10], 16), oct=7).pause(4,16).mpan(mrot(16))
 
 b1 >> blip(chords, dur=cascara, sus=linvar([.3,3],16), oct=5).pause(4,16,8).mpan(mrot(24))
@@ -332,6 +358,8 @@ change_bpm(140, True, 0.22)
 
 Root.default = var(PTri(12), .25)
 
+Root.default = 0
+
 #overwrite preceding hh
 m2 >> play(".--(-*-[**])", sdb=1, sample=0, hpf=2000, dur=1/4, leg=10, pan=P[-1,0,1].stutter(3)).pause(2,7).mpan(mrot(24))
 
@@ -343,6 +371,9 @@ k2 >> kicker("<(VVV(V[.V]V[VV]))(...V)>", dur=.5, amp=1, crush=8, bits=linvar([6
 
 k1.fadeout()
 k2.fadeout()
+
+k1.stop()
+k2.stop()
 
 k3 >> play(
     '<X.><v.>',
@@ -358,7 +389,7 @@ k3.sampfadeout(16)
 
 k3.only()
 
-k3.dur = Pvar(["<.X><.v>", '.', '<X.><v.>', '.'], [44, 4]),
+k3.degree = Pvar(["<.X><.v>", '.', '<X.><v.>', '.'], [44, 4]),
 
 k3.sample = var([1,4,0],48)
 k3.room2 = linvar([0,1], 16, start=Clock.mod(4))
@@ -378,8 +409,6 @@ b1.reso=0
 b1.env_mod=1
 b1.decay=0
 b1.oct=5
-
-b1.
 
 b1.cutoff=linvar([0,1,.4,.8,0,.6],PRand(2,24), start=Clock.mod(4))
 
@@ -402,11 +431,13 @@ k4.fadeout(32)
 
 k4.stop()
 
+k3.fadeout(16)
+
 d3 >> play("<(-~)[~-]>", dur=.5, amplify=pa16_2).mpan(mrot(16))
 
 k3 >> play("V", dur=clave23, sample=P[0,1,2].stutter(5), amplify=pa32, amp=1, output=12)
 
-b2 >> bbass([0,2,4,-5,2], dur=cascara, oct=var([3,4,5,6,7]), amp=2) + P(0,2)
+b2 >> bbass([0,2,4,-5,2], dur=cascara, oct=4), amp=2) + P(0,2)
 
 e3 >> play("#", dur=8, bits=4, cut=1/4, room=1, crush=8, shape=0.5, pan=[-1,-1,1], slide=-1)
 
@@ -415,6 +446,7 @@ d7 >> play("<..O...o.>", dur=.5, sample=var([0,1,2], 32), rate=linvar([.8,1.2], 
 
 d8 >> play("/", dur=16, pan=[-1, 0, -1]).mpan(mrot(32))
 
+k3.sampfadeout(8)
 
 b1 >> blip([0,2,0,0,-2,2], dur=[.25,.5], oct=6, amp=[1,0,0,0,1,0], sus=1).mpan(0)
 b2 >> blip([0,2,0,0,-2,2], dur=.5, oct=4, amp=[0,1,0,0,0,0]).mpan(1)
@@ -423,6 +455,8 @@ b3 >> blip([0,2,0,0,-2,2], dur=.5, oct=3, amp=[0,0,1,0,0,0]).mpan(2)
 b4 >> blip([0,2,0,0,-2,2], dur=[.75,.5], oct=7, amp=[0,1,0,1,0,0]).mpan(3).pause(4,24,12)
 b5 >> blip([0,2,0,0,-2,2], dur=.5, oct=5, amp=[0,0,0,0,1,0]).mpan(4)
 b6 >> blip([0,2,0,0,-2,2], dur=.5, oct=4, amp=[0,0,1,0,0,1]).mpan(5).pause(4,16)
+
+b_all.amplify=3
 
 # k3 >> kicker("<X.o.><v...>", lpf=00, sample=(2,4), amp=1.5, sdb=1, dur=1/4)
 k3 >> kicker(Pvar(["<.X><.v>",'.','<X.><v.>','.'], [44,4]), dur=1/2, sdb=1, sample=1, amp=1.5)
@@ -434,6 +468,8 @@ k3.leg=0
 k3.room2 = 1
 k3.mpan((0,1))
 
+
+bpm_to()
 
 k3.sampfadeout(32)
 k3.only()
@@ -473,6 +509,8 @@ k1 >> play(Pvar(["<.X><.v>",'.','<X.><v.>','.'], [44,4]), dur=1/2, sample=1, amp
 
 d3 >> play("<...c...c.><...(...*)><..(i).>", dur=1/4, rate=var([1,1.3]), sample=1, amp=3)
 
+d3.amplify=2
+
 b3 >> blip(pitches, dur=P[.5,.5,.25,.5,.25,.5,.25,.5,.5,.25]*1, sus=linvar([.1,2],32), amp=3).mpan(mrot(16))
 
 d2 >> play(".---", sdb=1, sample=0, hpf=2000, dur=1/4, leg=3, amp=2).pause(2,7)
@@ -491,6 +529,15 @@ b3 >> blip(pitches, dur=P[.5,.5,.25,.5,.25,.5,.25,.5,.5,.25]*1, sus=b1.dur-.04, 
 
 d2 >> play(".---", sdb=1, sample=0, hpf=2000, dur=1/4, leg=3, amp=2).pause(2,7)
 
-bpm_to(60,32)
+bpm_to(160,32)
+
+change_bpm(120, True, 0.22)
 
 Clock.clear()
+
+b1 >> blip(0, dur=.5).mpan(mrot(16)) + (P[0,2,-2,4] + P(0,2))
+
+d1 >> play("(x[xx]).-c", dur=.25)
+
+
+
